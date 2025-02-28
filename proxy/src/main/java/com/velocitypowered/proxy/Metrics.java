10c04492278bd84f220c3d9833e3dc166c1e15c9
404Setup <153366651+404Setup@users.noreply.github.com>
2025-02-28 20:06:27 +0800

    Simplify Metrics Collection

--- proxy/src/main/java/com/velocitypowered/proxy/Metrics.java
+++ proxy/src/main/java/com/velocitypowered/proxy/Metrics.java
@@ -104,6 +104,42 @@ public class Metrics {
 
     private static final Logger logger = LogManager.getLogger(Metrics.class);
 
+    private static final Map<String, Map<String, Integer>> JAVA_VERSIONS = new HashMap<>();
+
+    static {
+      getJavaVersions();
+    }
+
+    private static void getJavaVersions() {
+      String javaVersion = System.getProperty("java.version");
+      Map<String, Integer> entry = new HashMap<>();
+      entry.put(javaVersion, 1);
+
+      // http://openjdk.java.net/jeps/223
+      // Java decided to change their versioning scheme and in doing so modified the
+      // java.version system property to return $major[.$minor][.$security][-ea], as opposed to
+      // 1.$major.0_$identifier we can handle pre-9 by checking if the "major" is equal to "1",
+      // otherwise, 9+
+      String majorVersion = javaVersion.split("\\.")[0];
+      String release;
+
+      int indexOf = javaVersion.lastIndexOf('.');
+
+      if (majorVersion.equals("1")) {
+        release = "Java " + javaVersion.substring(0, indexOf);
+      } else {
+        // of course, it really wouldn't be all that simple if they didn't add a quirk, now
+        // would it valid strings for the major may potentially include values such as -ea to
+        // denote a pre release
+        Matcher versionMatcher = Pattern.compile("\\d+").matcher(majorVersion);
+        if (versionMatcher.find()) {
+          majorVersion = versionMatcher.group(0);
+        }
+        release = "Java " + majorVersion;
+      }
+      JAVA_VERSIONS.put(release, entry);
+    }
+
     @SuppressWarnings("all")
     static void startMetrics(VelocityServer server, VelocityConfiguration.Metrics metricsConfig) {
       Metrics metrics = new Metrics(logger, 24934, metricsConfig.isEnabled());
@@ -112,7 +148,7 @@ public class Metrics {
           new SingleLineChart("players", server::getPlayerCount)
       );
       metrics.addCustomChart(
-          new SingleLineChart("managed_servers", () -> server.getAllServers().size())
+          new SingleLineChart("managed_servers", () -> server.getServerCount())
       );
       metrics.addCustomChart(
           new SimplePie("online_mode",
@@ -120,41 +156,11 @@ public class Metrics {
       );
       metrics.addCustomChart(new SimplePie("velocity_version",
           () -> server.getVersion().getVersion()));
-      metrics.addCustomChart(new SimplePie("plugins", () -> String.valueOf(server.getPluginManager().getPluginCount())));
       metrics.addCustomChart(new SimplePie("native_mode",
               () -> Natives.compress.getLoadedVariant() != "Java" || Natives.cipher.getLoadedVariant() != "Java" ? "Yes" : "No"));
 
       metrics.addCustomChart(new DrilldownPie("java_version", () -> {
-        Map<String, Map<String, Integer>> map = new HashMap<>();
-        String javaVersion = System.getProperty("java.version");
-        Map<String, Integer> entry = new HashMap<>();
-        entry.put(javaVersion, 1);
-
-        // http://openjdk.java.net/jeps/223
-        // Java decided to change their versioning scheme and in doing so modified the
-        // java.version system property to return $major[.$minor][.$security][-ea], as opposed to
-        // 1.$major.0_$identifier we can handle pre-9 by checking if the "major" is equal to "1",
-        // otherwise, 9+
-        String majorVersion = javaVersion.split("\\.")[0];
-        String release;
-
-        int indexOf = javaVersion.lastIndexOf('.');
-
-        if (majorVersion.equals("1")) {
-          release = "Java " + javaVersion.substring(0, indexOf);
-        } else {
-          // of course, it really wouldn't be all that simple if they didn't add a quirk, now
-          // would it valid strings for the major may potentially include values such as -ea to
-          // denote a pre release
-          Matcher versionMatcher = Pattern.compile("\\d+").matcher(majorVersion);
-          if (versionMatcher.find()) {
-            majorVersion = versionMatcher.group(0);
-          }
-          release = "Java " + majorVersion;
-        }
-        map.put(release, entry);
-
-        return map;
+        return JAVA_VERSIONS;
       }));
     }
   }
